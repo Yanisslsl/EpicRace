f08de14a7bd26691698dd3117e63177a912f610a
Yaniss <yaniss@wikodit.fr>
2022-04-15 12:39:24 +0200

    feat: identity

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+using App.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Data;
+
+public class AppDbContext : IdentityDbContext
+{
+    public DbSet<Race> Races { get; set; }
+    public DbSet<RaceResult> RaceResults { get; set; }
+    public DbSet<ResultItem> ResultItem { get; set; }
+    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    
+    }
+}
