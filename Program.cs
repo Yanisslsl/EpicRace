38ee19534fea3581917583943b2c440a8d82cf98
Yaniss <yaniss@wikodit.fr>
2022-04-26 00:44:29 +0200

    fix

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddScoped<IRepository<Race>, EFRaceRepository>();
 
-var connectionString = "server=localhost;user=root;password=my_secret_psw;database=app_db";
+var connectionString = "server=localhost;user=root;password=root_password;database=app_db";
 var serverVersion = new MySqlServerVersion(new Version(8, 0, 27));
 
 
